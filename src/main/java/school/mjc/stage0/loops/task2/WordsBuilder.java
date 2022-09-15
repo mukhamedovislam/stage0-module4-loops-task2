a4acc1362fcf5cc473888b8b4880e33ef122f294
mukhamedovislam <bozuy24@gmail.com>
2022-09-15 08:06:28 +0400

    Add Loops Solutions #3

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
 	    String str = "";
-	    for(int i = 0; i <= chars.length(); i++){
+	    for(int i = 0; i <= chars.length; i++){
 		    str += chars[i];
 	    }
 	    System.out.println(str);
