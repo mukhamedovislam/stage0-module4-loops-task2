1ccbba1872decdbd7b96745de4916149119e3adb
mukhamedovislam <bozuy24@gmail.com>
2022-09-15 08:18:55 +0400

    Add Loops Solutions #4

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,11 +3,11 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
 	if(multiplyByAndToInclusive < 0){
-		for(int i = 0; i > multiplyByAndToInclusive; i--){
-			System.out.println(i*multiplyByAndToInclusive);
+		for(int i = 0; i >= multiplyByAndToInclusive; i--){
+			System.out.println((i*multiplyByAndToInclusive)*-1);
 		}
 	} else{
-		for(int i = 0; i < multiplyByAndToInclusive; i++){
+		for(int i = 0; i <= multiplyByAndToInclusive; i++){
 		System.out.println(i*multiplyByAndToInclusive);
 		}
 	}
